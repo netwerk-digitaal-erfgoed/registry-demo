cfd2963801f5f100c4ede5ea463524cca275f634
Bob Coret <bob.coret+github@gmail.com>
2021-10-01 16:51:59 +0200

    toevoeging slogan en typo

--- web/index.php
+++ web/index.php
@@ -3,7 +3,9 @@
    <section class="text m-t-space m-b-space m-theme--blue">
       <div class="o-container o-container__small m-t-space">
          <h1 class="title--l">Datasetregister</h1>
-         <p>Het datasetregister geeft inzicht (voor geïnteresseerden, onderzoekers, andere erfgoedinstellingen en softwareontwikkelaars) in de beschikbaarheid van datasets in het erfgoedveld en stimuleert daarmee het gebruik van deze datasets.</p>
+		 <h2 class="title--m am-text-align--right">Voor alle erfgoeddatasets!</h2>
+         <p><br></p>
+		 <p>Het datasetregister geeft inzicht (voor geïnteresseerden, onderzoekers, andere erfgoedinstellingen en softwareontwikkelaars) in de beschikbaarheid van datasets in het erfgoedveld en stimuleert daarmee het gebruik van deze datasets.</p>
          <hr class="hr--thick m-theme-before no-theme item-in-view inview">
       </div>
    </section>
@@ -37,7 +39,7 @@
             <div class="all-1_3 phablet-1_1 item-in-view inview">
                <div class="c-grid__col m-text-align--center">
                   <div class="text m-t-half-space">
-                     <h3>Ontwikel je software voor erfgoedinstellingen?</h3>
+                     <h3>Ontwikkel je software voor erfgoedinstellingen?</h3>
                      <ul>
                         <li><a target="triplestore" href="https://netwerk-digitaal-erfgoed.github.io/requirements-datasets/">Eisen gesteld aan datasetbeschrijvingen</a></li>
                         <li><a href="/validate.php">Valideer een datasetbeschrijving</a></li>
