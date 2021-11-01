23cb27a7b9e13df29a9960355f60def4db9630c7
Bob Coret <bob.coret+github@gmail.com>
2021-11-01 14:04:05 +0100

    update color footer links (wcag issue)

--- web/includes/header.php
+++ web/includes/header.php
@@ -10,7 +10,7 @@
          <link rel="icon" href="assets/favicon-32x32.png">
          <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0 user-scalable=no">
          <meta http-equiv="X-UA-Compatible" content="IE=edge">
-         <link rel="stylesheet" id="nde-style-css" href="assets/style-20211020.css" type="text/css" media="all">
+         <link rel="stylesheet" id="nde-style-css" href="assets/style-20211101.css" type="text/css" media="all">
          <link rel="stylesheet" id="site-style-css" href="assets/site-20211020.css" type="text/css" media="all">
          <link rel="apple-touch-icon" sizes="180x180" href="assets/apple-touch-icon.png">
          <link rel="icon" type="image/png" sizes="32x32" href="assets/favicon-32x32.png">
