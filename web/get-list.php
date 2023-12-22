b7c973f54dd75041cd381a8df8b6fc21515c01af
Bob Coret <bob.coret+github@gmail.com>
2023-12-22 20:01:40 +0100

    make loading of lists (SPARQL query) asynchronous
    Not a real solution for #20 but at least is looks faster in the GUI (because loading isn't blocking)

---
+++ web/get-list.php
@@ -0,0 +1,26 @@
+<?php
+
+include("includes/search.php");
+
+if (isset($_GET["list"])) {
+	if ($_GET["list"]=="publishers") {
+		$list=getPublishers();
+	} elseif ($_GET["list"]=="creators") {
+		$list=getCreators();
+	} elseif ($_GET["list"]=="formats") {
+		$list=getFormats();
+	} else {
+		exit;
+	}
+	
+	# client cache 2 hours
+	$now = time();
+	$generatedAt = gmdate('D, d M Y H:i:s T', $now);
+	$lastModified = gmdate('D, d M Y 00:00:00 T', $now);
+	$expiresAt = gmdate('D, d M Y H:i:s T', strtotime($lastModified) + 2*3600); # 2 hours
+	header('Last-modified: ' . $lastModified);
+	header('Cache-control: max-age=' . strtotime($expiresAt) - strtotime($generatedAt));
+
+	header('Content-Type: application/json; charset=utf-8');
+	echo json_encode($list);
+}
