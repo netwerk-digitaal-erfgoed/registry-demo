650084e4690713eab786bb372ed4f262212a7409
Bob Coret <bob.coret+github@gmail.com>
2025-12-01 00:28:19 +0100

    Reset validation results on failure
    Reset strValidationResults to an empty string when validation fails.

--- web/validate.php
+++ web/validate.php
@@ -306,6 +306,8 @@ function displayMessages(response) {
 
 		strValidationResults += " <?= t('geconstateerd') ?></h2>";
 		showMessages(arrMessages);
+	} else {
+		strValidationResults = "";
 	}
 
 	document.getElementById("api_result").innerHTML = strValidationResults;
